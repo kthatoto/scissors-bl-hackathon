222c5e871cd86f9a8c48d8353d9195d545ca4814
kthatoto <kthatoto@gmail.com>
2018-02-27 05:51:58 +0900

    Use semantic ui style

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,11 @@ module Scissors
       g.helper         false
       g.test_framework false
     end
+    config.to_prepare do
+      Devise::SessionsController.layout "devise"
+      Devise::RegistrationsController.layout proc{ |controller| user_signed_in? ? "application" : "devise" }
+      Devise::UnlocksController.layout "devise"
+      Devise::PasswordsController.layout "devise"
+    end
   end
 end
