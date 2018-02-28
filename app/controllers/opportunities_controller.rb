47a7f65010501fa11fab540c741223eec8124b7b
kthatoto <kthatoto@gmail.com>
2018-02-28 13:44:42 +0900

    Added seaerch

--- app/controllers/opportunities_controller.rb
+++ app/controllers/opportunities_controller.rb
@@ -1,5 +1,10 @@
 class OpportunitiesController < ApplicationController
 
   def search
+    from_date = DateTime.now
+    to_date   = params[:date].to_i.days.since
+    opp_arel = Opportunity.arel_table[:date]
+    @opportunities = Opportunity.all
+    # @opportunities = Opportunity.where(opp_arel.gteq(from_date).and(opp_arel.lteq(to_date)))
   end
 end
